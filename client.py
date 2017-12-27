412a19cea34e5591bcd7c8b4eac9bf8fc2f66545
Morichill <33785411+Morichill@users.noreply.github.com>
2017-12-27 11:12:58 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,68 @@
+import socket
+
+class ChatClient(object):
+    """ A command line chat client using select """
+
+    def __init__(self, name, port, host=SERVER_HOST):
+        self.name = name
+        self.connected = False
+        self.host = host
+        self.port = port
+        # Начальное приглашение
+        self.prompt='[' + '@'.join((name, socket.gethostname().split('.')[0])) + ']> '
+        # Подключиться к серверу по порту
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((host, self.port))
+            print ("Now connected to chat server@ port %d" % self.port)
+            self.connected = True
+            # Отправка моего имени...
+            send(self.sock,'NAME: ' + self.name)
+            data = receive(self.sock)
+            # Содержит адрес клиента, устаналиваем его
+            addr = data.split('CLIENT: ')[1]
+            self.prompt = '[' + '@'.join((self.name, addr)) + ']> '
+        except socket.error as e:
+            print ("Failed to connect to chat server @ port %d" % self.port)
+            sys.exit(1)
+
+def run(self):
+    """ Chat client main loop """
+    while self.connected:
+        try:
+            sys.stdout.write(self.prompt)
+            sys.stdout.flush()
+            # Ожидаем ввод из stdin и сокета
+            readable, writeable,exceptional = select.select([0, self.sock], [],[])
+            for sock in readable:
+                if sock == 0:
+                    data = sys.stdin.readline().strip()
+                    if data: send(self.sock, data)
+                elif sock == self.sock:
+                    data = receive(self.sock)
+                    if not data:
+                        print ('Client shutting down.')
+                        self.connected = False
+                        break
+                    else:
+                        sys.stdout.write(data + '\n')
+                        sys.stdout.flush()
+        except KeyboardInterrupt:
+            print (" Client interrupted. """)
+            self.sock.close()
+break
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Socket Server Example with Select')
+    parser.add_argument('--name', action="store", dest="name", required=True)
+    parser.add_argument('--port', action="store", dest="port", type=int, required=True)
+    given_args = parser.parse_args()
+    port = given_args.port
+    name = given_args.name
+    if name == CHAT_SERVER_NAME:
+        server = ChatServer(port)
+        server.run()
+    else:
+        client = ChatClient(name=name, port=port)
+        client.run()
