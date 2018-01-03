dc4a0286b21ec7e852ace54a40d57a15126204b6
Morichill <33785411+Morichill@users.noreply.github.com>
2018-01-03 12:52:07 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,50 @@
+import server
+import client
+
+
+def simple_func(value):
+ #отлавливаем ошибки с помощью Assert
+    assert type(value) == int   
+    assert value > 0
+
+    return value*value		
+def test_server_starts_tcp_server(self):
+    # Start server 
+    srv = server.server('127.0.0.1', 7777)
+    server_thread = threading.Thread(target=srv.start_listening)
+    server_thread.start()
+
+    # On my computer, 0.0000001 is the minimum sleep time or the 
+    # client might connect before server thread binds and listens
+    # Other computers will differ. I wanted a low number to make tests fast
+    time.sleep(0.000001)  
+
+    # This is our fake test client that is just going to attempt a connect and disconnect
+    fake_client = socket.socket()
+    fake_client.settimeout(1)
+    fake_client.connect(('127.0.0.1', 7777))
+    fake_client.close()
+
+    # Make sure server thread finishes
+    server_thread.join()
+
+def run_fake_server(self):
+    # Run a server to listen for a connection and then close it
+    server_sock = socket.socket()
+    server_sock.bind(('127.0.0.1', 7777))
+    server_sock.listen(0)
+    server_sock.accept()
+    server_sock.close()
+
+def test_client_connects_and_disconnects_to_default_server(self):
+    # Start fake server 
+    server_thread = threading.Thread(target=self.run_fake_server)
+    server_thread.start()
+
+    # Test the clients basic connection and disconnection
+    client = client.client()
+    client.connect('127.0.0.1', 7777)
+    client.disconnect()
+
+    # Ensure server thread ends
+    server_thread.join()
