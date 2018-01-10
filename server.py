b9b02059223a59b96ada13eaaa8d2d1e7c4dbd66
Morichill <33785411+Morichill@users.noreply.github.com>
2018-01-11 00:01:17 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-
+  
 import select
 import socket
 import sys
@@ -12,7 +12,7 @@ CHAT_SERVER_NAME = 'server'
 
 
 class ChatServer(object):
-def __init__(self, port, backlog=5):
+    def __init__(self, port, backlog=5):
         self.clients = 0
         self.clientmap = {}
         self.outputs = [] # прослушиваем сокеты вывода
@@ -23,7 +23,7 @@ def __init__(self, port, backlog=5):
         self.server.listen(backlog)
         # Перехватываем прерывания клавиатуры
         signal.signal(signal.SIGINT, self.sighandler)
-
+    
     def sighandler(self, signum, frame):
         """ Clean up client outputs"""
         # Закрываем данный сервер
@@ -39,65 +39,65 @@ def __init__(self, port, backlog=5):
         host, name = info[0][0], info[1]
         return '@'.join((name, host))
 			
-def run(self):
-    inputs = [self.server, sys.stdin]
-    self.outputs = []
+   def run(self):
+       inputs = [self.server, sys.stdin]
+       self.outputs = []
 
-    running = True
-    while running:
-        try:
-        readable, writeable, exceptional = select.select(inputs, self.outputs, [])
-    except select.error as e:
-        break
+       running = True
+       while running:
+           try:
+           readable, writeable, exceptional = select.select(inputs, self.outputs, [])
+       except select.error as e:
+           break
 
-    for sock in readable:
-        if sock == self.server:
-            # обрабатываем сокет данного сервера
-            client, address = self.server.accept()
-            print ("Chat server: got connection %d from %s" %(client.fileno(), address))
-            # Считываем имя регистрации
-            cname = receive(client).split('NAME: ')[1]
-            # Вычислем имя клиента и отправляем обратно
-            self.clients += 1
-            send(client, 'CLIENT: ' + str(address[0]))
-            inputs.append(client)
-            self.clientmap[client] = (address, cname)
-            # Отправляем присоединённую информацию прочим клиентам
-            msg = "\n(Connected: New client (%d) from %s)" %(self.clients, self.get_client_name(client))
-            for output in self.outputs:
-                send(output, msg)
-            self.outputs.append(client)
+       for sock in readable:
+           if sock == self.server:
+               # обрабатываем сокет данного сервера
+               client, address = self.server.accept()
+               print ("Chat server: got connection %d from %s" %(client.fileno(), address))
+               # Считываем имя регистрации
+               cname = receive(client).split('NAME: ')[1]
+               # Вычислем имя клиента и отправляем обратно
+               self.clients += 1
+               send(client, 'CLIENT: ' + str(address[0]))
+               inputs.append(client)
+               self.clientmap[client] = (address, cname)
+               # Отправляем присоединённую информацию прочим клиентам
+               msg = "\n(Connected: New client (%d) from %s)" %(self.clients, self.get_client_name(client))
+               for output in self.outputs:
+                   send(output, msg)
+               self.outputs.append(client)
 
-        elif sock == sys.stdin:
-            # обрабатываем стандартный ввод
-            junk = sys.stdin.readline()
-            running = False
-        else:
-            # обрабатываем все прочие сокеты
-            try:
-                data = receive(sock)
-                if data:
-                    # Отправляем как новое сообшение клиента...
-                    msg = '\n#[' + self.get_client_name(sock)
-                                 + ']>>' + data
-                    # Отправляем данные всем за исключением себя
-                    for output in self.outputs:
-                        if output != sock:
-                            send(output, msg)
-                        else:
-                            print ("Chat server: %d hung up" % sock.fileno())
-                            self.clients -= 1
-                            sock.close()
-                            inputs.remove(sock)
-                            self.outputs.remove(sock)
+           elif sock == sys.stdin:
+               # обрабатываем стандартный ввод
+               junk = sys.stdin.readline()
+               running = False
+           else:
+               # обрабатываем все прочие сокеты
+               try:
+                   data = receive(sock)
+                   if data:
+                       # Отправляем как новое сообшение клиента...
+                       msg = '\n#[' + self.get_client_name(sock)
+                                    + ']>>' + data
+                       # Отправляем данные всем за исключением себя
+                       for output in self.outputs:
+                           if output != sock:
+                               send(output, msg)
+                           else:
+                               print ("Chat server: %d hung up" % sock.fileno())
+                               self.clients -= 1
+                               sock.close()
+                               inputs.remove(sock)
+                               self.outputs.remove(sock)
 
-                            # Отправляем оставленную клиентом иформацию остальным
-                            msg = "\n(Now hung up: Client from %s)" %self.get_client_name(sock)
-                            for output in self.outputs:
-                                send(output, msg)
-            except socket.error as e:
-                # Удаляем
-                inputs.remove(sock)
-                self.outputs.remove(sock)
-self.server.close()			
+                               # Отправляем оставленную клиентом иформацию остальным
+                               msg = "\n(Now hung up: Client from %s)" %self.get_client_name(sock)
+                               for output in self.outputs:
+                                   send(output, msg)
+               except socket.error as e:
+                   # Удаляем
+                   inputs.remove(sock)
+                   self.outputs.remove(sock)
+   self.server.close()			
 	
